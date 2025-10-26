ed4943cf7ac893fbdd5a21df33f5ffef950cbe85
mxpadidar <mxpadidar@gmail.com>
2025-10-26 20:24:43 +0330

    setup fastapi application

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
 import logging
 
+import uvicorn
+from fastapi import FastAPI
+
 from app.config import settings
 
 logging.basicConfig(level=settings.LOG_LEVEL)
@@ -7,8 +10,18 @@ logging.basicConfig(level=settings.LOG_LEVEL)
 logger = logging.getLogger(__name__)
 
 
+app = FastAPI()
+
+
 def main() -> None:
     logger.info(f"Starting {settings.APP_NAME} application...")
+    uvicorn.run(
+        "app.main:app",
+        host="127.0.0.1",
+        port=settings.PORT,
+        reload=settings.DEBUG,
+        log_config=None,
+    )
 
 
 if __name__ == "__main__":
