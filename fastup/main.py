91f1f34520950205986932ded0904ebca4ca269a
mxpadidar <mxpadidar@gmail.com>
2025-10-31 14:53:29 +0330

    feat(fastapi): run fastapi app with uvicorn

--- fastup/main.py
+++ fastup/main.py
@@ -1,7 +1,15 @@
 import logging
 import logging.config
 
+import uvicorn
+from fastapi import FastAPI
+
 from fastup import config
+
+app = FastAPI(
+    title=config.APP["name"],
+    version=config.APP["version"],
+)
 
 
 def main() -> None:
@@ -9,6 +17,14 @@ def main() -> None:
 
     logging.config.dictConfig(config.LOG_CONFIG)
     logging.getLogger("main").info("Application starting...")
+
+    uvicorn.run(
+        "fastup.main:app",
+        host=config.SERVER["host"],
+        port=config.SERVER["port"],
+        reload=config.SERVER["debug"],
+        log_config=None,
+    )
 
 
 if __name__ == "__main__":
