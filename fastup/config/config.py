c20e59c4e80527dd137a4c33a6c6cdd56e9f0e66
mxpadidar <mxpadidar@gmail.com>
2025-10-31 09:22:37 +0330

    feat(confile): load confile.toml

--- fastup/config/config.py
+++ fastup/config/config.py
@@ -1,6 +1,13 @@
+import pathlib
+
+from .parsers import parse_toml_file
 from .types import AppConf
 
-APP: AppConf = {
-    "name": "FastUp",
-    "version": "0.1.0",
-}
+root_dir = pathlib.Path(__file__).parent.parent.parent
+
+try:
+    confile = parse_toml_file(root_dir / "confile.toml")
+except ValueError as e:  # pragma: no cover
+    raise RuntimeError(f"Error parsing config file: {e}")
+
+APP: AppConf = confile["app"]
