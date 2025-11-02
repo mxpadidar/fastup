cf36d2aac9a4d6b775de69f5db799c187cddb775
mxpadidar <mxpadidar@gmail.com>
2025-11-03 02:48:53 +0330

    feat(mapper): orm mapper for tables and entities

--- fastup/adapters/database/__init__.py
+++ fastup/adapters/database/__init__.py
@@ -1,7 +1,9 @@
 from .engine import async_engine, async_session_factory, mapper_registry
+from .mapper import start_orm_mapper
 
 __all__ = [
     "async_engine",
     "async_session_factory",
     "mapper_registry",
+    "start_orm_mapper",
 ]
