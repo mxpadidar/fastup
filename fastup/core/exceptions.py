ca53710f666f73303691164b42501cde200b8fd7
mxpadidar <mxpadidar@gmail.com>
2025-11-20 23:09:40 +0330

    feat(exceptions): custom domain exceptions

---
+++ fastup/core/exceptions.py
@@ -0,0 +1,23 @@
+import typing
+
+
+class BaseExc(Exception):
+    message: str = "An unexpected application error occurred."
+    extra: dict[str, typing.Any]
+
+    def __init__(self, message: str | None = None, extra: dict | None = None) -> None:
+        self.message = message or self.message
+        self.extra = extra or dict()
+        super().__init__(self.message)
+
+
+class InternalExc(BaseExc):
+    message = "An unexpected internal error occurred."
+
+
+class NotFoundExc(BaseExc):
+    message = "The requested resource could not be found."
+
+
+class ConflictExc(BaseExc):
+    message = "A resource conflict occurred, likely due to a duplicate entry."
