bd16506840f8836793f55ded99566f9695971490
mxpadidar <mxpadidar@gmail.com>
2025-11-25 01:53:46 +0330

    feat(repo): add get by phone to user repo

--- fastup/infra/sql_repositories/user_sql_repo.py
+++ fastup/infra/sql_repositories/user_sql_repo.py
@@ -1,5 +1,7 @@
 import typing
 
+import sqlalchemy
+
 from fastup.core.entities import User
 from fastup.core.repositories import UserRepo
 
@@ -12,3 +14,11 @@ class UserSQLRepo(SQLRepository[User], UserRepo):
     @property
     def entity_cls(self) -> typing.Type:  # pragma: no cover
         return User
+
+    async def get_by_phone(self, phone: str, only_active: bool = True) -> User | None:
+        """Retrieve a user by phone number, with an option to include deleted users."""
+        stmt = sqlalchemy.select(self.entity_cls).where(self.entity_cls.phone == phone)
+        if only_active:
+            stmt = stmt.where(self.entity_cls.deleted_at.is_(None))
+        result = await self.session.execute(stmt)
+        return result.scalar_one_or_none()
